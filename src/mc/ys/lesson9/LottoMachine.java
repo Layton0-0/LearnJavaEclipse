a1cd044f01e144162e9b0733c802495641a06e1f
Yunjeong Choi / 최윤정 <yeshan0224@naver.com>
2022-01-10 18:22:25 +0900

    실수로 삭제한 오류 수정 완

--- src/mc/ys/lesson9/LottoMachine.java
+++ src/mc/ys/lesson9/LottoMachine.java
@@ -7,16 +7,17 @@ public class LottoMachine {
 	LottoMachine(ArrayList<LottoBall> balls){
 		this.balls = balls;
 	}
+	
 
 	void startMachine() {
-		System.out.println("로또 번호를 뽑습니다.\n");
+		System.out.println("추첨을 시작하겠습니다.\n");
 		Integer[] choiceBalls = new Integer[6];
 		
 		for(int count = 0; count < 6; count++) {
 			LottoBall ball = this.getBall();
 
 			choiceBalls[count] = ball.getBallNumber();
-			System.out.println(ball.getBallNumber() + "번이 선택되었습니다.\n");
+			System.out.println(ball.getBallNumber() + "踰덉씠 �꽑�깮�릺�뿀�뒿�땲�떎.\n");
 
 			try {
 				Thread.sleep(1000);
@@ -25,7 +26,7 @@ public class LottoMachine {
 			}
 		}
 		Arrays.sort(choiceBalls);
-		System.out.println("제 224회차 추첨 번호는 " + Arrays.toString(choiceBalls));
+		System.out.println("�젣 224�쉶李� 異붿꺼 踰덊샇�뒗 " + Arrays.toString(choiceBalls));
 		
 	}
 	
@@ -45,7 +46,7 @@ public class LottoMachine {
 		Random r = new Random();
 		int idx = r.nextInt(balls.size());
 		ball = balls.remove(idx);
-		System.out.println("로또 볼을 하나 선택했습니다.");
+		System.out.println("濡쒕삉 蹂쇱쓣 �븯�굹 �꽑�깮�뻽�뒿�땲�떎.");
 		return ball;
 	}
 	
