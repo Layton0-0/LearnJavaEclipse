fa50f36d2229a59ecb98ecdb346a147f7ec2d736
Yunjeong Choi / 최윤정 <92416563+Layton0-0@users.noreply.github.com>
2022-01-11 18:44:40 +0900

    과제 제출 완료

---
+++ JavaHomework/JavaProject2/Magazine.java
@@ -0,0 +1,21 @@
+package javaProject2;
+
+public class Magazine extends Book {
+	int month;
+	
+	public Magazine(String bookNo, String bookTitle, String bookAuthor, int bookPrice, int bookYear,
+			String bookPublisher, int month) {
+		super(bookNo, bookTitle, bookAuthor, bookPrice, bookYear, bookPublisher);
+		this.month = month;
+	}
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return super.toString() + "\t " + Integer.toString(month);
+	}
+}
